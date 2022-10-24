ced7e4c741bd052035570df88373a8205eaacf47
achnitreda <achnitreda@gmail.com>
2022-10-24 10:17:50 +0100

    add user model

--- models/role.model.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({ name: "string" });
-const Role = mongoose.model("Role", schema);
-
-module.exports = Role;
---
+++ models/userModel.js
@@ -0,0 +1,63 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bycrpt = require("bcryptjs");
+
+// name, email, password, passwordConfirm
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true /* Making sure that the email is unique. */,
+    lowercase: true /* transform email to lowercase. */,
+    validate: [validator.isEmail, "Please enter a valid email"],
+  },
+  // role: {
+  //   type: String,
+  //   enum: ['user', 'guide', 'lead-guide', 'admin'],
+  //   default: 'user'
+  // },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: 8,
+    /* Making sure that the password is not being sent back to the client. */
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      // this is only work with create and save
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
+
+/* The above code is a pre-save hook. It is a function that runs before a document is saved to the
+database. */
+userSchema.pre("save", async function (next) {
+  // Only run this fn if pass is modified or created "save"
+  if (!this.isModified("password")) return next();
+
+  // hash the pass with cost of 12
+  this.password = await bycrpt.hash(this.password, 12);
+
+  // delete passConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPass = async function (cadidatePass, userPass) {
+  return await bycrpt.compare(cadidatePass, userPass);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
